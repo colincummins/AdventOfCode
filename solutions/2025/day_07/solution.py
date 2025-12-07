405ac90121ee5ff13d1520b645b38f3f6e8fc222
Colin Cummins/usr/local/bin/python3 <84826067+colincummins@users.noreply.github.com>
2025-12-07 00:42:22 -0800

    Retool Pt1 to entirely bit based

--- solutions/2025/day_07/solution.py
+++ solutions/2025/day_07/solution.py
@@ -3,11 +3,22 @@
 # puzzle prompt: https://adventofcode.com/2025/day/7
 
 from ...base import StrSplitSolution, answer
+from collections import deque
 
 
 class Solution(StrSplitSolution):
     _year = 2025
     _day = 7
+
+    def parseInput(self):
+        buffer = []
+        for row in self.input:
+            num = 0
+            for i, node in enumerate(row):
+                if node != ".":
+                    num |= 1 << i
+            buffer.append(num)
+        self.input = buffer
 
     # @answer(1234)
     def part_1(self) -> int:
@@ -19,17 +30,26 @@ class Solution(StrSplitSolution):
 
     #@answer((1234, 4567))
     def solve(self) -> tuple[int, int]:
+        self.parseInput()
+        print(self.input)
+        maxDepth = len(self.input) - 1
+        print(maxDepth)
         split = 0
-        curr = 1 << (70)
-        for row in self.input:
+        q = deque()
+        q.append((0, self.input[0]))
+        #clear first row so it's not interpreted as a reflector
+        self.input[0] = 0
+        while q:
             buffer = 0
-            for i, node in enumerate(row):
-                if node == "^" and (curr & 1 << i):
-                    buffer |= (1 << i + 1) 
-                    buffer |= (1 << i - 1)
-                    curr ^= 1 << i
-                    split += 1
-            curr |= buffer
-            print("current",str(bin(buffer))[2:].zfill(30))
+            depth, row = q.popleft()
+            print(depth)
+            collisions = row & self.input[depth]
+            split += collisions.bit_count()
+            row ^= collisions
+            row |= collisions << 1
+            row |= collisions >> 1
+            if depth < maxDepth:
+                q.append((depth + 1, row))
+
         return (split, 0)
 
