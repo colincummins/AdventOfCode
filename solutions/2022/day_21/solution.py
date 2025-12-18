5bbd56f586e1580315b9e870247d218fc2041bdf
Colin Cummins <all.our.august.sundays@gmail.com>
2025-12-18 13:00:07 -0800

    Passes Part 1

--- solutions/2022/day_21/solution.py
+++ solutions/2022/day_21/solution.py
@@ -4,65 +4,100 @@
 
 from ...base import StrSplitSolution, answer
 import operator
+from collections import defaultdict, deque
+
+class Monkey():
+    def __init__(self, key: str, phrase: str):
+        self.key = key
+        self.num = None
+        self.waiting1 = self.waiting2 = None
+
+        if phrase.isnumeric():
+            self.inDegree = 0
+            self.num = int(phrase)
+            return
+
+        self.inDegree = 2
+        self.waiting1, self.operation, self.waiting2 = phrase.split(" ")
+        match self.operation:
+            case "+": 
+                self.operation = operator.add
+            case "-": 
+                self.operation = operator.sub
+            case "*": 
+                self.operation = operator.mul
+            case "/": 
+                self.operation = operator.truediv
+
+    def __hash__(self):
+        return hash(self.key)
+
+    def listen(self, key: str, value: int):
+        self.inDegree -= 1
+        self.__setattr__(key, value)
+
+        if self.inDegree == 0:
+            self.num = self.operation(self.__getattribute__(self.waiting1), self.__getattribute__(self.waiting2))
+
+
+    def shout(self) -> tuple[str, int]:
+        if self.num is None:
+            raise ValueError("Monkey doesn't have needed nums")
+        return self.key, self.num
+
+    def getWaiting(self) -> tuple[str, str]:
+        return (self.waiting1, self.waiting2) if self.waiting1 and self.waiting2 else [] 
+
+    def __repr__(self):
+        return self.key
+
 
 
 class Solution(StrSplitSolution):
     _year = 2022
     _day = 21
 
-    class Monkey():
-        def __init__(self, key: str, phrase: str):
-            self.val1 = self.val2 = None
-            self.key = key.strip(":")
-            self.number = None
-
-            if phrase.isnumeric():
-                self.inDegree = 0
-                self.number = int(phrase)
-                return
-
-            self.inDegree = 2
-            self.neighbor1, self.operation, self.neighbor2 = phrase.split(" ")
-            match self.operation:
-                case "+": 
-                    self.operation = operator.add
-                case "-": 
-                    self.operation = operator.sub
-                case "*": 
-                    self.operation = operator.mul
-                case "/": 
-                    self.operation = operator.truediv
-
-        def __hash__(self):
-            return self.key
-
-        def listen(self, key: str, value: int):
-            if self.neighbor1 == key:
-                self.val1 = value
-                self.inDegree -= 1
-            elif self.neighbor2 == key:
-                self.val2 = value
-                self.inDegree -= 1
-            else:
-                raise KeyError
-
-            if self.val1 is not None and self.val2 is not None:
-                self.number = self.operation(self.val1, self.val2)
-
-        def shout(self) -> tuple[str, int]:
-            if self.number is None:
-                raise ValueError("Monkey cannot calculate number")
-            return self.key, self.number
-
-        def getNeighbors(self) -> tuple[str, str]:
-            return self.neighbor1, self.neighbor2
+        
 
 
 
 
     # @answer(1234)
     def part_1(self) -> int:
-        pass
+        neighbors = defaultdict(list)
+        jungle = {}
+        for line in self.input:
+            key, phrase = line.split(": ")
+            jungle[key] = Monkey(key, phrase)
+        print("Jungle:", jungle.values())
+        for monkey in jungle.values():
+            for neighbor in monkey.getWaiting():
+                neighbors[neighbor].append(monkey)
+        print("Neighbors:", neighbors)
+
+        q = deque(filter(lambda x: x.inDegree == 0, jungle.values()))
+        print("Start queue:", q)
+
+        while q:
+            curr = q.popleft()
+            print("Current Monkey", curr)
+            print("Their neighbors:", neighbors[curr.key])
+            for neighbor in neighbors[curr.key]:
+                print("{} shouts {} to {}".format(*curr.shout(), neighbor.key))
+                neighbor.listen(*curr.shout())
+                if neighbor.inDegree == 0:
+                    print("Neighbor {} has calculated their number: {}".format(neighbor.key, neighbor.num))
+                    q.append(neighbor)
+
+        print("Final queue:", q)
+
+        return jungle["root"].num
+
+
+            
+
+
+
 
     # @answer(1234)
     def part_2(self) -> int:
