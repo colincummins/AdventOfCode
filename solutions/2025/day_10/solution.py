2f2fa7f86a8eb7702c7c78646b2260175cbad38e
Colin Cummins/usr/local/bin/python3 <84826067+colincummins@users.noreply.github.com>
2025-12-22 20:10:50 -0800

    Working on vector division

--- solutions/2025/day_10/solution.py
+++ solutions/2025/day_10/solution.py
@@ -43,6 +43,14 @@ class Solution(StrSplitSolution):
 
         return buttons, joltages
 
+    @staticmethod
+    def divideArray(numerator, denominator):
+        if ~((denominator == 0) & (numerator > 0)).any() and (numerator % denominator == 0).all():
+            return numerator[0] // denominator[0]
+        
+        return -1
+
+
 
     @answer(401)
     def part_1(self) -> int:
@@ -65,20 +73,34 @@ class Solution(StrSplitSolution):
 
     # @answer(1234)
     def part_2(self) -> int:
+        divisible = array([2, 1, 1, 2])
+        divisibleZero = array([0, 1, 1, 2])
+        indivisible = array([3, 1, 1, 2])
+        combo = array([2, 1, 1, 2])
+        comboZero = array([0, 1, 1, 2])
+        print(self.divideArray(divisible, combo))
+        print(self.divideArray(indivisible, combo))
+        print(self.divideArray(indivisible, comboZero))
+        print(self.divideArray(divisibleZero, comboZero))
+
         part2solution = 0
         for line in self.input:
             buttons, joltages = self.parseLine2(line)
 
-            buttonCombos = [a + b for a, b in combinations(buttons, 2)]
+            buttonCombos = [array(a + b) for a, b in combinations(buttons, 2)]
             self.debug("2-Button Combos", buttonCombos)
 
             @cache
             def recNumPresses(remainingJoltage: tuple[int]) -> int:
+                remainingJoltage = array(remainingJoltage)
+
                 if any([x < 0 for x in remainingJoltage]):
                     return inf
                 if all([x == 0 for x in remainingJoltage]):
                     return 0
 
+                for combo in buttonCombos:
+                    print(combo)
 
             assert recNumPresses((0,0,0,0)) == 0
             assert recNumPresses((-1,0,0,0)) == inf
@@ -86,6 +108,8 @@ class Solution(StrSplitSolution):
             recNumPresses.cache_clear()
             self.debug("New Line:")
             self.debug(buttons, joltages)
+
+            recNumPresses(joltages)
 
         return part2solution 
 
